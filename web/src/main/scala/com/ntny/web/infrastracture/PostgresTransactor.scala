9cd54f7f8b260578ab1c47bc65f49ca506203bfd
anton <Anton.Pechenin@nordigy.ru>
2021-03-13 02:40:45 +0300

    improve transactor creation

--- web/src/main/scala/com/ntny/web/infrastracture/PostgresTransactor.scala
+++ web/src/main/scala/com/ntny/web/infrastracture/PostgresTransactor.scala
@@ -26,23 +26,4 @@ object PostgresTransactor {
       xa <- HikariTransactor.fromHikariConfig[F](config, ce, blocker)
     } yield xa
   }
-
-  implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
-
-  def transactor(): Resource[IO, HikariTransactor[IO]] = {
-
-
-    for {
-      ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
-      be <- Blocker[IO]
-      xa: HikariTransactor[IO] <- HikariTransactor.newHikariTransactor[IO](
-        "org.postgresql.Driver",
-        "jdbc:postgresql://localhost:5432/postgres",
-        "postgres",
-        "password123",
-        ce,
-        be
-      )
-    } yield xa
-  }
 }
